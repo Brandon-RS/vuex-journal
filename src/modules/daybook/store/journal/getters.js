c01982858b4cc67317c6a58bc9e83c66bddde3fc
Brandon Rojas <brandon.rsantillan@gmail.com>
2022-03-06 23:29:10 -0500

    Frontend done

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,7 +1,12 @@
-export const getEntriesByTerm = (/* state */) => {
-  // TODO: description!
+export const getEntriesByTerm = (state) => (term = '') => {
+  if (term.length === 0) {
+    return state.entries
+  }
+  return state.entries.filter(entry => entry.text.toLowerCase().includes(term.toLowerCase()))
 }
 
-export const getEntrieById = (/* state */) => {
-  // TODO: description!
+export const getEntryById = (state) => (id = '') => {
+  const entry = state.entries.find(entry => entry.id === id)
+  if (!entry) return
+  return { ...entry } // Por valor y no por referencia.
 }
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,19 +2,19 @@ export default () => ({
   isLoading: true,
   entries: [
     {
-      id: new Date().getTime(),
+      id: '1',
       date: new Date().toDateString(),
       text: 'Lorem ullamco Lorem aliquip quis quis enim. Ex et incididunt voluptate labore excepteur fugiat officia nisi. Tempor nostrud laboris magna pariatur nisi fugiat. Elit ex laboris fugiat ad nulla id et magna fugiat id excepteur dolor exercitation aute.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 100,
+      id: '2',
       date: new Date().toDateString(),
       text: 'Voluptate anim labore ad enim officia dolor dolore in. Aute ullamco cupidatat ut do velit nostrud excepteur aliquip irure est cupidatat elit. Veniam ut reprehenderit proident excepteur nostrud officia id. Commodo tempor est aliqua voluptate ea dolor reprehenderit cupidatat veniam veniam amet mollit. Ullamco nostrud duis dolor dolore incididunt adipisicing ut.',
       picture: null,
     },
     {
-      id: new Date().getTime() + 200,
+      id: '3',
       date: new Date().toDateString(),
       text: 'Deserunt enim consectetur qui officia anim id aliquip deserunt. Labore ea est nulla sint incididunt enim aliqua mollit. Esse et culpa ad excepteur culpa incididunt aliqua elit cillum magna veniam. Laborum ex ipsum occaecat reprehenderit pariatur duis nostrud non adipisicing dolore. Incididunt sunt nisi magna deserunt. Voluptate ut excepteur pariatur dolore. Non anim nulla nulla esse veniam proident sit exercitation elit tempor velit anim pariatur.',
       picture: null,
